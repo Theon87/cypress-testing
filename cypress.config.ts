3d33dd7df45e33e32fec1f757263778406dcb2f0
theon87 <theonfrancis17@gmail.com>
2024-11-05 09:13:32 -0500

    update base Url

--- cypress.config.ts
+++ cypress.config.ts
@@ -13,6 +13,6 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://localhost:3001",
+    baseUrl: "http://localhost:3000",
   },
 });
