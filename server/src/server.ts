cf8db121803f2f7c606ae77542e92e077086a5b3
theon87 <theonfrancis17@gmail.com>
2024-11-05 16:40:57 -0500

    update app global app route dirname path

--- server/src/server.ts
+++ server/src/server.ts
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 
    app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
   });
 }
 
