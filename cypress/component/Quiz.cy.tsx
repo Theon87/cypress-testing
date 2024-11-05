c1bab765aeb4e13a08305477d09fb9a0b4712ecc
theon87 <theonfrancis17@gmail.com>
2024-11-05 07:19:37 -0500

    add test to check that start quiz appears on load

--- cypress/component/Quiz.cy.tsx
+++ cypress/component/Quiz.cy.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Quiz from '../../client/src/components/Quiz'
 
-describe('<Quiz />', () => {
+describe('a button to start the quiz should appear on initial load', () => {
   it('renders', () => {
     cy.mount(<Quiz />)
+
+    cy.get('button').contains('contain', 'Start Quiz')
   })
 })
