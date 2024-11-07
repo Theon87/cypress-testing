a0dc3a232240e596307e5e8fcb36d114183850f9
theon87 <theonfrancis17@gmail.com>
2024-11-07 08:26:25 -0500

    deletes userJourney e2e test

--- cypress/e2e/userJourney.cy.ts
+++
@@ -1,8 +0,0 @@
-describe("Quiz Questions", () => {
-  it('should display a question on screen after the Start Quiz button is hit', () => {
-    cy.visit('http://localhost:3000')
-
-    cy.get('button').click()
-    cy.get('h2').should('exist')
-  })
-})
