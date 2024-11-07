208d040c1634e0c8c32700c3393234685a6c19a2
theon87 <theonfrancis17@gmail.com>
2024-11-07 08:03:01 -0500

    add e2e test to ensure data is retrieved when the start button is hit

--- cypress/e2e/quizQuestions.cy.ts
+++ cypress/e2e/quizQuestions.cy.ts
@@ -1,4 +1,12 @@
 describe('Quiz Questions', () => {
+
+  it('should display a card on thes screen if data is retrieved', () => {
+    cy.visit('http://localhost:3000')
+
+    cy.get('button').click()
+    cy.get('.card').should('be.visible')
+  })
+
   it('should display a question on screen after the Start Quiz button is hit', () => {
     cy.visit('http://localhost:3000')
 
